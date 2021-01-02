6f58d6168eeaa032b37fec1ca10375d8f84c0c10
Marcel Grum <1960484@stud.hs-mannheim.de>
2021-01-02 14:48:59 +0000

    add reset-dev.sh

---
+++ reset-dev.sh
@@ -0,0 +1,10 @@
+# This script will reset the local dev environment
+# It will also create a new python virtual environment and activate it
+source venv/bin/activate
+deactivate
+rm -r venv
+rm -r build
+find . | grep -E "(__pycache__|\.pyc|\.pyo$|*egg-info)" | xargs rm -rf
+find . -name "*.so" -type f -delete
+python3 -m venv venv
+source venv/bin/activate
