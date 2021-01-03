692844786712185de601dc53b7c5187277efb044
Marcel Grum <1960484@stud.hs-mannheim.de>
2021-01-03 00:21:52 +0000

    format setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from Cython.Build import cythonize
 
 setup(
-    name = "flask example app",
-    ext_modules = cythonize("flaskr/prime_cython.pyx"),
+    name="flask example app",
+    ext_modules=cythonize("flaskr/prime_cython.pyx"),
     packages=find_packages(),
 )
