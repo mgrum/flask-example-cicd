26515686177fb32057cb2c068d94f146d9cbfba4
mgrum <mgrum1994@gmail.com>
2021-01-05 01:55:48 +0000

    update scripts to upgrade pip

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 # This script will install dependencies and modules
+pip install pip --upgrade
 pip install -r requirements.txt
 pip install -e .
--- reset-dev.sh
+++ reset-dev.sh
@@ -4,9 +4,11 @@ source venv/bin/activate
 deactivate
 rm -r venv
 rm -r build
+rm -r dist
 rm -r .pytest_cache
 find . | grep -E "(__pycache__|\.pyc|\.pyo$|*egg-info)" | xargs rm -rf
 find . -name "*.so" -type f -delete
 python3 -m venv venv
 source venv/bin/activate
+pip install pip --upgrade
 pip install flake8 pytest pylint
