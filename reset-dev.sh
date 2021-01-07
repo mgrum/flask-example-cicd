1d0502b35477eb4af9bc85a4ab4adb3c401e0c0c
mgrum <mgrum1994@gmail.com>
2021-01-07 02:19:52 +0000

    update reset-dev

--- reset-dev.sh
+++ reset-dev.sh
@@ -1,15 +1,15 @@
 # This script will reset the local dev environment
 # It will also create a new python virtual environment and activate it
+echo "Deactivating venv if it exists..."
 source venv/bin/activate
 deactivate
-rm -r venv
-rm -r build
-rm -r dist
-rm -r .pytest_cache
-find . | grep -E "(__pycache__|\.pyc|\.pyo$|*egg-info)" | xargs rm -rf
-find . -name "*.so" -type f -delete
+echo "Deleting build and development files..."
+find . | grep -E "(venv|build|dist|__pycache__|.pytest_cache|\.pyc|\.pyd|\.pyo$|\.so|*egg-info)" | xargs rm -rf
+echo "Creating new virtual environment..."
 python3 -m venv venv
 source venv/bin/activate
+echo "Installing development dependencies..."
 pip install pip --upgrade
 pip install flake8 pylint autopep8 pytest
-# Now you should build the project manually or with ". build.sh"
+COL='\033[1;32m'
+echo -e "${COL}Now you can build the project manually or by using build.sh"
