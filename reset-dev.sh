09049681c2517d29ec47455c9df17d0e165cbe42
mgrum <mgrum1994@gmail.com>
2021-01-07 02:28:48 +0000

    update reset-dev fix

--- reset-dev.sh
+++ reset-dev.sh
@@ -4,12 +4,13 @@ echo "Deactivating venv if it exists..."
 source venv/bin/activate
 deactivate
 echo "Deleting build and development files..."
-find . | grep -E "(venv|build|dist|__pycache__|.pytest_cache|\.pyc|\.pyd|\.pyo$|\.so|*egg-info)" | xargs rm -rf
+find . -type d | grep -E "(venv|build|dist|__pycache__|.pytest_cache|egg-info)" | xargs rm -rf
+find . -type f | grep -E "(\.pyc|\.pyd|\.pyo$|\.so)" | xargs rm -rf
 echo "Creating new virtual environment..."
 python3 -m venv venv
 source venv/bin/activate
 echo "Installing development dependencies..."
 pip install pip --upgrade
 pip install flake8 pylint autopep8 pytest
-COL='\033[1;32m'
-echo -e "${COL}Now you can build the project manually or by using build.sh"
+COLOR='\033[1;32m'
+echo -e "${COLOR}Now you can build the project manually or by using build.sh"
