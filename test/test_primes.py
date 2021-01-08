40493d2977307a2a4506f98a4543ebd32a912e08
mgrum <mgrum1994@gmail.com>
2021-01-08 00:28:07 +0000

    improve test coverage

--- test/test_primes.py
+++ test/test_primes.py
@@ -1,3 +1,5 @@
+import pytest
+
 from flaskr import prime_cython
 
 
@@ -8,3 +10,8 @@ def test_primes():
     expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
     result = prime_cython.primes(10)
     assert expected == result
+    
+def test_primes_wrong_input():
+    assert [] == prime_cython.primes(-1)
+    with pytest.raises(TypeError):
+        prime_cython.primes("1")
